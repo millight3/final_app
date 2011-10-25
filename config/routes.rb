9c6c19b47b4da68196f40fa076aab5ff0fb138c7
millight3 <millight@gmail.com>
2011-10-25 01:26:53 -0700

    done with app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FinalApp::Application.routes.draw do
+  get "home/index"
+
+  resources :movies
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ FinalApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "movies#index"
 
   # See how all your routes lay out with "rake routes"
 
